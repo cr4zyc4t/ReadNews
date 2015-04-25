f06c8d50150f837bf4af70ee6dff2950db163110
cr4zyc4t <hunter9xbn@gmail.com>
2015-04-25 11:59:55 +0700

    add subcategory

--- app/src/main/java/toanvq/recyclerview/SubCategoryActivity.java
+++ app/src/main/java/toanvq/recyclerview/SubCategoryActivity.java
@@ -2,18 +2,32 @@ package toanvq.recyclerview;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
-public class SubCategoryActivity extends ActionBarActivity {
+public class SubCategoryActivity extends ActionBarActivity implements RecyclerView_Adapter.ItemClickListener {
     private final String FETCH_SUBCTG_URL = "http://content.amobi.vn/api/cafe24h/listsubcategory";
     private List<RecyclerView_Item> listSubCtg = new ArrayList<RecyclerView_Item>();
     private RecyclerView_Adapter adapter;
-
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +35,42 @@ public class SubCategoryActivity extends ActionBarActivity {
 
         int category_id = getIntent().getIntExtra("category_id", 0);
 
+        RecyclerView subctg_container = (RecyclerView) findViewById(R.id.subctg_container);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar2);
 
+        subctg_container.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new RecyclerView_Adapter(listSubCtg, this);
+        adapter.setItemClickListener(this);
+
+        subctg_container.setAdapter(adapter);
+
+        getSubCategory();
+    }
+
+    private void getSubCategory() {
+        progressBar.setVisibility(View.VISIBLE);
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, FETCH_SUBCTG_URL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("name", "Androidhive");
+                params.put("email", "abc@androidhive.info");
+                params.put("password", "password123");
+
+                return params;
+            }
+        };
     }
 
 
@@ -45,5 +94,10 @@ public class SubCategoryActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void ItemClicked(int position) {
+
     }
 }
